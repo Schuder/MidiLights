c1d065791eeadbb581bb306358055ab7346d7a51
TootsieRoller <Sheeper.sleep@gmail.com>
2014-08-27 12:15:06 -0500

    test commit
    tester

--- MidiLights.java
+++ MidiLights.java
@@ -3,7 +3,8 @@ public class MidiLights {
 	public static void main(String _args[]) {
 	
 		System.out.println("Yo.");
-	
+		System.out.println("KASHGJKLASGHKLA");
+		System.out.println("eli did this yaya");
 	}
 
 }
