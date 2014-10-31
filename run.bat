e59472b9d703728d105c897c55581a473b1b683b
TootsieRoller <Sheeper.sleep@gmail.com>
2014-10-31 12:33:31 -0500

    we guich
    dwas

--- run.bat
+++ run.bat
@@ -2,5 +2,6 @@ set PATH="C:\Program Files (x86)\Java\jdk1.6.0_26\bin"
 REM set PATH="C:\Program Files\Java\jre1.8.0_25\bin"
 javac LitesEmulator.java
 javac TrackListing.java
+javac MidiGUI.java
 java MidiGUI
-rem pause
+REM pause
